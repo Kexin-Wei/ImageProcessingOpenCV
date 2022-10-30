a46e4dc4644069cb1a1df058241aea0075240781
Kexin-Wei <wkx611@outlook.com>
2022-10-30 23:00:01 +0800

    init push

---
+++ opencvCPPtest/CMakeFiles/OpencvTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpencvTest.dir/OpencvTest.cpp.o"
+  "CMakeFiles/OpencvTest.dir/OpencvTest.cpp.o.d"
+  "OpencvTest"
+  "OpencvTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpencvTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
