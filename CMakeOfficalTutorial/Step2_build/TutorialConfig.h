e1c27c62e9e5bd24206f5bacca989699c27311ed
Kexin-Wei <wkx611@outlook.com>
2023-03-01 22:29:33 +0800

    cmake offical tutorial 1-4 done

--- CMakeOfficalTutorial/Step2_build/TutorialConfig.h
+++ CMakeOfficalTutorial/Step2_build/TutorialConfig.h
@@ -1,4 +1,6 @@
 // the configured options and settings for Tutorial
 #define Tutorial_VERSION_MAJOR 1
 #define Tutorial_VERSION_MINOR 0
-/* #undef USE_MYMATH */
+
+// TODO 13: use cmakedefine to define USE_MYMATH
+#define USE_MYMATH
