19f5321194607ac02eb135cd4cb267b45e381d38
weikexin611 <wei.kexin@ultrastmedtech.com>
2023-03-02 14:44:59 +0800

    change to cmake 3.25 and update to Step5

--- CMakeOfficalTutorial/Step1_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
+++ CMakeOfficalTutorial/Step1_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/kk/ImageProcessingOpenCV/CMakeOfficalTutorial/Step1/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
+  "/mnt/d/GitRepos/ImageProcessingOpenCV/CMakeOfficalTutorial/Step1/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
   )
 
 # Targets to which this target links.
--- CMakeOfficalTutorial/Step3_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
+++ CMakeOfficalTutorial/Step3_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -8,11 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/kk/ImageProcessingOpenCV/CMakeOfficalTutorial/Step3/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
+  "/mnt/d/GitRepos/ImageProcessingOpenCV/CMakeOfficalTutorial/Step3/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/d/GitRepos/ImageProcessingOpenCV/CMakeOfficalTutorial/Step3_build/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
