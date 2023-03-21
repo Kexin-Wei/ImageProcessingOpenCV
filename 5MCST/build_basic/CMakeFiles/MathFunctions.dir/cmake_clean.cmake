4e45637b6ddcd8c5e0849827d7187a1351f8ef78
Kexin-Wei <wkx611@outlook.com>
2023-03-21 08:42:52 +0800

    win: add 5MCST as a backup

---
+++ 5MCST/build_basic/CMakeFiles/MathFunctions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathFunctions.dir/util/sqrt.cpp.o"
+  "CMakeFiles/MathFunctions.dir/util/sqrt.cpp.o.d"
+  "libMathFunctions.a"
+  "libMathFunctions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
